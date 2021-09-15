448f5d9954d5dd3eb79dbb21c9a140876b32aac5
solehfuddin@gmail.com <solehfuddin@gmail.com>
2021-09-15 23:18:00 +0700

    logo dan splash screen

---
+++ app/src/main/java/com/sofudev/submissionpemula/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.sofudev.submissionpemula
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val imgSplash : ImageView = findViewById(R.id.image_splash)
+        imgSplash.alpha = 0f
+        imgSplash.animate().setDuration(3000).alpha(2f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
